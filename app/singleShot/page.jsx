5d7fc589985da32f887f5206f36947672485de94
DebojyotiFloydee <debojyoti@floydee.com>
2024-06-19 14:24:52 +0530

    cleanup

--- app/singleShot/page.jsx
+++ app/singleShot/page.jsx
@@ -1,37 +1,48 @@
-'use client'
+"use client";
 
 import Link from "next/link";
 import React, { useState, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { addImage } from "./imageSlice";
 import { Camera } from "react-camera-pro";
+import { nanoid } from "@reduxjs/toolkit";
 
 function SingleShot() {
-    const camera = useRef(null);
-    // const [image, setImage] = useState(null);
+  const camera = useRef(null);
+  // const [image, setImage] = useState(null);
 
-    const [counter, setCounter] = useState(0);
-    const incrementCounter = () => setCounter(counter + 1);
+  const [counter, setCounter] = useState(0);
+  const incrementCounter = () => setCounter(counter + 1);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
+  const addImageHandler = (img, counter) => {
+    dispatch(addImage({ id: nanoid(), src: img, counter: counter }));
+    incrementCounter();
+  };
 
-    const addImageHandler = (img, counter) => {
-        dispatch(addImage({ imgsrc: img, counter: counter }));
-        incrementCounter();
-    }
-
-    return (
-        <div>
-            <div className="overlay"></div>
-            <Camera ref={camera} aspectRatio={'cover'} facingMode='environment' />
-            <div className="button-capture-wrapper">
-                <button className="button button-capture" title="Capture" onClick={() => addImageHandler(camera.current.takePhoto(), counter)}>{counter}</button>
-                <Link className="button button-finish" title="Finish" href={{ pathname: "/main/" }}>&nbsp;</Link>
-            </div>
-
-        </div>
-    )
+  return (
+    <div>
+      <div className="overlay"></div>
+      <Camera ref={camera} aspectRatio={"cover"} facingMode="environment" />
+      <div className="button-capture-wrapper">
+        <button
+          className="button button-capture"
+          title="Capture"
+          onClick={() => addImageHandler(camera.current.takePhoto(), counter)}
+        >
+          {counter}
+        </button>
+        <Link
+          className="button button-finish"
+          title="Finish"
+          href={{ pathname: "/main/" }}
+        >
+          &nbsp;
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default SingleShot
+export default SingleShot;
